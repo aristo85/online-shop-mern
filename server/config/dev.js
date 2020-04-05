ba8abdd0393505084da2084526ca081e9b2bf013
aristo85 <42740909+aristo85@users.noreply.github.com>
2020-04-06 03:12:56 +0700

    Delete dev.js

--- server/config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://aristo85:123456Abc@cluster0-shard-00-00-ehted.mongodb.net:27017,cluster0-shard-00-01-ehted.mongodb.net:27017,cluster0-shard-00-02-ehted.mongodb.net:27017/tests?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
-    SESSION_SECRET: 'pojgiesdf'
-}
